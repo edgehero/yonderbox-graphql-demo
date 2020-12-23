547c06484909cc3e474b8ec079935c9dbbb80322
Joris Röling <joris@bonboa.com>
2020-12-23 12:11:48 +0100

    test WIP

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const { ApolloClient, gql } = require('apollo-boost')
 const { InMemoryCache } = require('apollo-cache-inmemory')
 const { createHttpLink } = require('apollo-link-http')
+const { JSONPath } = require('jsonpath-plus');
 
 
 const graphQL = new ApolloClient({
@@ -21,6 +22,77 @@ describe('Tests the Yonderbox GraphQL MongoDB Adapter', () => {
   it('should handle the most basic query "query {__typename}"', async() => {
     const query = gql `query {__typename}`
     await expect(graphQL.query({query})).resolves.toMatchObject({data: {__typename: 'Query'}})
+  })
+  it('should handle the more complex query', async() => {
+    const query = gql `
+query juke($filter: Object) {
+  introspect {
+    name
+    version
+    meta
+  }
+  juke {
+    collections(_filter: {slug: "juke-app-home"}) {
+      title
+      slug
+      items(_filter: $filter) {
+        ... on Juke_Section {
+          title
+          images {
+            ...imageFragment
+          }
+        }
+      }
+    }
+  }
+}
+
+fragment imageFragment on Juke_Image {
+  _id
+  image {
+    _id
+    file {
+      url
+    }
+    description
+  }
+}
+`
+
+    const variables = {
+	"filter": {
+		"title": {
+			"$regex": "radio",
+			"$options": "i"
+		}
+	}
+}
+
+    const match = {
+      data: {
+        juke: {
+          collections: [
+            expect.objectContaining(
+              {
+                items: [
+                  expect.objectContaining(
+                    {
+                      title: expect.stringMatching('radio')
+                    }
+                  )
+                ]
+              }
+            )
+          ]
+        }
+      }
+    }
+/*    await expect(graphQL.query({query,variables})).resolves.toMatchObject({data: {__typename: 'Query'}  })*/
+/*    await expect(graphQL.query({query,variables})).resolves.toEqual(expect.objectContaining(match))*/
+    await expect(graphQL.query({query,variables})).resolves.toMatchObject(match)
+
+/*const result = JSONPath({path: '...', json});*/
+
   })
   /*  it('should successfully create a user with valid credentials', async() => {
     const createUser = gql `
