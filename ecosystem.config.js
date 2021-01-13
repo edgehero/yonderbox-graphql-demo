550148248a5ca5b28dab5104adaec45b3a3b0a94
Joris Röling <joris@bonboa.com>
2021-01-13 10:06:17 +0100

    ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  apps : [{
+    name: 'yonderbox-graphql-demo',
+    script: 'index.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: '--max-http-header-size=32768',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : 'vps2.bonboa.com',
+      ref  : 'origin/master',
+      repo : 'git@github.com:jorisroling/yonderbox-graphql-demo.git',
+      path : '/home/joris/Projects/yonderbox-graphql-demo',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
